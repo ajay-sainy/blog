195f17735b30bd9ad95d1836836e4e82d98c1db6
= <sainyajay@gmail.com>
2016-07-16 16:00:04 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+  // Initialize materialScrollTop
+  $.material.init();
+  $('body').materialScrollTop();
+
+  // $("a[href*=#]").click(function(e) {
+  //   e.preventDefault();
+  // });  
+  
+});
